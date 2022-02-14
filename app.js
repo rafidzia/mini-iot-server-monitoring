baccb40b0cf624c60e574765e104e3a85ab7045f
rafidzia <32030497+rafidzia@users.noreply.github.com>
2022-02-14 14:01:58 +0700

    push to github

--- app.js
+++ app.js
@@ -1,70 +1,94 @@
-const mqtt      = require("mqtt");
-const express   = require("express");
-const app       = express();
-const mongoose  = require("mongoose");
-const events   = require("events");
-var http        = require("http").Server(app);
-var io          = require("socket.io")(http);
-var ee          = new events.EventEmitter();
-const httpPort  = 8091;
+const mqtt = require("mqtt");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const events = require("events");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var ee = new events.EventEmitter();
+const httpPort = 8094;
 
 app.use('/assets', express.static('assets'));
 
-// var conn = mongoose.createConnection('mongodb+srv://cluster0.jpi1b.mongodb.net/wannalog', 
-// {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true,
-//     "auth": {
-//         "authSource": "admin"
-//     },
-//     "user": "rafidzia",
-//     "pass": "asd018-dsa",
-// });
-
-// var temp = conn.model("temp", {
-//     adc : Number,
-//     opamp : Number,
-//     lm35 : Number,
-//     temp : Number,
-//     led : String,
-//     time : String
-// })
-
-var client = mqtt.connect("mqtt://broker.hivemq.com", {clientId : "mqttjsfarid1"});
-
-client.on("connect",()=>{	
-    if(client.connected) console.log("mqtt connected");
-    client.subscribe("/mofuban/wannalog", {qos : 1});
+var conn = mongoose.createConnection('mongodb+srv://cluster0.jpi1b.mongodb.net/wannalog', 
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    "auth": {
+        "authSource": "admin"
+    },
+    "user": "rafidzia",
+    "pass": "asd018-dsa",
 });
 
-client.on("error",(error)=>{ 
+var temp = conn.model("temp", {
+    adc : Number,
+    opamp : Number,
+    lm35 : Number,
+    temp : Number,
+    led : String,
+    time : String,
+    speed : Number
+})
+
+var client = mqtt.connect("mqtt://broker.emqx.io", { clientId: "mqttjsfarid1" });
+
+client.on("connect", () => {
+    if (client.connected) console.log("mqtt connected");
+    client.subscribe("/mofuban/wannalog", { qos: 1 });
+    client.subscribe("/mofuban/ledstat", { qos: 1 });
+});
+
+client.on("error", (error) => {
     console.log("Can't connect" + error);
     process.exit(1);
 });
 
-client.on('message',function(topic, message, packet){
-    var jsondata = JSON.parse(message);
-    ee.emit("newtemp", jsondata);
-    // const newdata = new temp(JSON.parse(message));
-    // newdata.save((err, result)=>{
-    //     if(err) throw err;
-    //     console.log(result)
-    // })
-});
-
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", (socket)=>{
-    console.log("ws client connected");
-    socket.emit("tes", "tes");
-    ee.on("newtemp", (data)=>{
-        socket.emit("newtemp", JSON.stringify(data));
-    })
+client.on('message', function (topic, message, packet) {
+    if(topic == "/mofuban/wannalog"){
+        var jsondata = JSON.parse(message);
+        ee.emit("newtemp", jsondata);
+        message = JSON.parse(message);
+        if(message.save){
+            const newdata = new temp(message);
+            newdata.save((err, result)=>{
+                if(err) throw err;
+            })
+        }
+    }
+    if(topic == "/mofuban/ledstat"){
+        ee.emit("newledstat", String(message));
+    }
     
 });
 
-http.listen(process.env.PORT || httpPort, function(){
+clientPublish = (topic, data)=>{
+    if (client.connected == true){
+        client.publish(topic, data, {retain:false, qos:1})
+    }
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("ws client connected");
+    socket.emit("tes", "tes");
+    ee.on("newtemp", (data) => {
+        socket.emit("newtemp", JSON.stringify(data));
+    })
+    socket.on("btnled", ()=>{
+        clientPublish("/mofuban/ledtoggle", "");
+    })
+    ee.on("newledstat", (data)=>{
+        socket.emit("newledstat", data);
+    })
+    socket.on("alarmset", (data)=>{
+        clientPublish("/mofuban/alarmset", data);
+    })
+});
+
+http.listen(process.env.PORT || httpPort, function () {
     console.log('Server listen port ' + httpPort);
 });
--- assets/socketclient.js
+++ assets/socketclient.js
@@ -1,8 +1,17 @@
 
 var tempArr = [];
 
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(onpageload);
+
+function changeledstat(data){
+    var ledstat = document.getElementById("led-status");
+    if(Number(data)){
+        ledstat.innerText = "ON";
+    }else{
+        ledstat.innerText = "OFF";
+    }
+}
 
 function drawChart() {
     var head = [["time", "temp (°C)"]];
@@ -14,7 +23,7 @@ function drawChart() {
         curveType: 'function',
         legend: { position: 'bottom' },
         hAxis: {
-            format : "HH:mm"
+            format: "HH:mm:ss"
         }
     };
 
@@ -23,16 +32,44 @@ function drawChart() {
     chart.draw(data, options);
 }
 var socket = io();
-socket.on("tes", function(data){
+socket.on("tes", function (data) {
     console.log(data);
 })
 
-function onpageload(){
-    socket.on("newtemp", function(data){
+function onpageload() {
+    socket.on("newtemp", function (data) {
         var jsondata = JSON.parse(data);
+        document.getElementById("temp").innerHTML = jsondata.temp + " &#8451;";
+        document.getElementById("fan").innerHTML = jsondata.speed + " %";
+        changeledstat(jsondata.ledbt);
         var newarr = [new Date(jsondata.time), jsondata.temp]
         tempArr.push(newarr);
+        if (tempArr.length == 38) {
+            tempArr.shift();
+        }
         drawChart();
     })
 }
+
+window.onload = function(){
+
+    var btnled = document.getElementById("btn-led");
+    btnled.onclick = function(){
+        socket.emit("btnled")
+    }
+
+    var formalarm = document.getElementById("form-alarm");
+    var inputalarm = document.getElementById("input-alarm");
+    formalarm.onsubmit = function(e){
+        e.preventDefault();
+        socket.emit("alarmset", inputalarm.value);
+        inputalarm.value = "";
+    }
+    
+}
+
+socket.on("newledstat", function(data){
+    changeledstat(data);
+})
+
 
